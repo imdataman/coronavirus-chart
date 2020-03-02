206a4670bf43ea119a37d77f1ab106c20c113e70
Andy Lin <>
2020-03-02 15:53:25 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChinaMap :csv="csv"/>
+    <WorldMap :csv="csv"/>
+    <TaiwanMap :csv="csv"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChinaMap from './components/ChinaMap.vue'
+import WorldMap from './components/WorldMap.vue'
+import TaiwanMap from './components/TaiwanMap.vue'
+
+const d3 = Object.assign({}, require('d3-request'))
 
 export default {
   name: 'App',
+  data () {
+    return {
+      csv: false
+    }
+  },
+  mounted () {
+      d3.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-01-2020.csv", d => {
+        this.csv = d
+      })
+  },
   components: {
-    HelloWorld
+    ChinaMap,
+    WorldMap,
+    TaiwanMap,
   }
 }
 </script>
